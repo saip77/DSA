3e63c3e06f2591c7e16296628d857a1a5d4e66f3
SAI PRASAD <sp12961369@gmail.com>
2026-03-11 23:48:14 +0530

    LC-448

---
+++ Day-03/03-numbers-disappeared-in-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int>ans;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            while(nums[i]<=n && nums[nums[i]-1]!=nums[i])
+            {
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+        for(int i=0; i<n; i++){
+        if(nums[i]!=i+1)
+         ans.push_back(i+1);
+        }
+        return ans;
+    }
+};
